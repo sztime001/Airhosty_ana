0cc56d555e3644a22b11fa0749f909c7d4a380a7
sztime001 <sofeiw0908@gmail.com>
2018-05-27 22:17:24 +1000

    code entry

--- Analysis1_Property.py
+++ Analysis1_Property.py
@@ -1,1 +1,57 @@
+#1. import 
+import pandas as pd
+import numpy as np
+import time
+import datetime
 
+listings_file = 'C:\\Users\\sofei\\Dropbox\\A Sofei\\CS\\Airhosty\\Data\\data.csv'
+
+#import columns we want to extract
+columns = ['Reservation Code',
+           'Listing ID',
+           'Full Address',
+           'Booking User ID',
+           'Checkin Date',
+           'Checkout Date',
+           'Nights',
+           'Guests',
+           'Guest Phones',
+           'Base Price',
+           'Extrac',
+           'Fee',
+           'Tax',
+           'Total']
+listing = pd.read_csv(listings_file, usecols=columns)
+
+#rename columns
+listing.columns = ['Reservation_Code',
+           'Listing_ID',
+           'Full_Address',
+           'Booking_User_ID',
+           'Checkin_Date',
+           'Checkout_Date',
+           'Nights',
+           'Guests',
+           'Guest—_Phones',
+           'Base_Price',
+           'Extrac',
+           'Fee',
+           'Tax',
+           'Total']
+
+#2. cleaning up the data
+# replacing NaN values with 0
+listing.fillna(0, inplace=True)
+
+#separating date column into day month and year
+listing['Checkin_Year'],listing['Checkin_Month'],listing['Checkin_Day']=listing['Checkin_Date'].str.split('-',2).str
+listing['Checkout_Year'],listing['Checkout_Month'],listing['Checkout_Day']=listing['Checkout_Date'].str.split('-',2).str
+
+#sort data by Listing ID and Checkin Date
+listing = listing.sort_values(by = ['Listing_ID','Checkin_Date'])
+
+#group data by Listing ID
+property_info = listing.groupby(['Listing_ID', 'Full_Address']).agg({'Checkin_Date': 'min','Checkout_Date': 'max'})
+property_info = property_info.rename(columns = {'Checkin_Date':'Come_into_Market','Checkout_Date':'End_of_Sta'})
+
+#3. analisis of occupancy
